b0d06272488396683cef126859ce0ea8cf2e4bc8
unknown <care9000@naver.com>
2020-02-12 08:46:52 +0900

    2020-02-12|14890(경사로)commit's

--- 13459/13459_구슬_탈출.py
+++ 13459/13459_구슬_탈출.py
@@ -3,51 +3,68 @@ sys.stdin = open('13459_구슬_탈출.txt')
 
 
 def dfs(red_i, red_j, blue_i, blue_j, cnt):
-    red_i_start = red_i
-    red_j_start = red_j
-    if result != 11:
+    global result
+
+    red_start_i = red_i
+    red_start_j = red_j
+    blue_start_i = blue_i
+    blue_start_j = blue_j
+    goal = 0
+    if cnt > 10:
         return
-    elif cnt > 10:
+    elif result == 1:
         return
 
     for k in range(4):
         for _ in range(10):
-            red_i_tem = red_i_start + dx[k]
-            red_j_tem = red_j_start + dy[k]
-            if ispass(red_i_tem, red_j_tem):
-                if mini_map[red_i_tem][red_j_tem] == 0:
-                    mini_map[red_i_tem][red_j_tem] = 7
-                    mini_map[red_i_start][red_j_start] = 0
-                    red_i_start = red_i_tem
-                    red_j_start = red_j_tem
+            for a in range(2):
+                if a == 0:
+                    i_tem = red_i + dx[k]
+                    j_tem = red_j + dy[k]
+                    if -1 < i_tem < N and -1 < j_tem < M:
+                        if mini_map[i_tem][j_tem] == 9:
+                            goal += 1
+                        elif mini_map[i_tem][j_tem] == 0:
+                            mini_map[red_i][red_j] = 0
+                            red_i = i_tem
+                            red_j = j_tem
+                            mini_map[i_tem][j_tem] = 7
+                else:
+                    pass
+        if goal == 1:
+            result = 1
+            return
+        elif goal > 1:
+            return
 
-                elif mini_map[red_i_tem][red_j_tem] == 9:
-                    goal = 1
-
-
-
-
-
-
-def ispass(i, j):
-    if -1 < i < N and -1 < j < M:
-        return True
-    return False
+        dfs(red_i, red_j, blue_i, blue_j, cnt + 1)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-N, M = map(int, input().split())
 
+N, M = map(int, input().split())
 dummy = [input() for _ in range(N)]
+
 mini_map = [[0 for _ in range(M)] for _ in range(N)]
+
+#빨간 구슬 7 파란구술 8 골인지점 9
+red_i = 0
+red_j = 0
+
+blue_i = 0
+blue_j = 0
 for i in range(N):
     for j in range(M):
         if dummy[i][j] == 'R':
             mini_map[i][j] = 7
+            red_i = i
+            red_j = j
 
         elif dummy[i][j] == 'B':
             mini_map[i][j] = 8
+            blue_i = i
+            blue_j = j
 
         elif dummy[i][j] == 'O':
             mini_map[i][j] = 9
@@ -55,18 +72,5 @@ for i in range(N):
         elif dummy[i][j] == '#':
             mini_map[i][j] = 1
 
-
-
-
-location = [[0, 0], [0, 0]]
-for i in range(N):
-    for j in range(M):
-        if mini_map[i][j] == 'R':
-            location[0][0] = i
-            location[0][1] = j
-        elif mini_map[i][j] == 'B':
-            location[1][0] = i
-            location[1][1] = j
-
-result = 11
-dfs(location[0][0], location[0][1], location[1][0], location[1][1], 0)
+result = 0
+dfs(red_i, red_j, blue_i, blue_j, 0)
