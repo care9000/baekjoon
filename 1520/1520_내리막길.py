fb0bf828d164d3ea5eb218f4babcdc5466c07880
unknown <care9000@naver.com>
2020-04-02 20:16:36 +0900

    2020-04-02|1520(내리막길)commit's

--- 1520/1520_내리막길.py
+++ 1520/1520_내리막길.py
@@ -1,17 +1,21 @@
 import sys
 sys.stdin = open('1520_내리막길.txt')
-import collections
 input = sys.stdin.readline
 
 def dfs(i, j):
     if i == M - 1 and j == N - 1:
-        dp[i][j] += 1
-        return
+        return 1
+    # 이미 왔었으면 더이상 할 필요 없음
+    if dp[i][j] != -1:
+        return dp[i][j]
+    # 방문 했으면 방문 체크를 0으로 해줌
+    dp[i][j] = 0
     for k in range(4):
         nx = i + dx[k]
         ny = j + dy[k]
-        if ispass(nx, ny) and mini_map[nx][ny] < mini_map[i][j]:
-            dfs(nx, ny)
+        if ispass(nx, ny) and mini_map[i][j] > mini_map[nx][ny]:
+            dp[i][j] += dfs(nx, ny)
+    return dp[i][j]
 
 
 def ispass(i, j):
@@ -28,7 +32,7 @@ mini_map = []
 for _ in range(M):
     data = list(map(int, input().split()))
     mini_map.append(data)
-dp = [[0 for _ in range(N)] for _ in range(M)]
-dp[0][0] = 1
+dp = [[-1 for _ in range(N)] for _ in range(M)]
 dfs(0, 0)
-print(dp[-1][-1])
+print(dp[0][0])
+
